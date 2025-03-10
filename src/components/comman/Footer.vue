e9c6036ff7cb30f5a6ad8041e7d1a68164a368f9
shreynesscale23 <shreywork23@gmail.com>
2025-03-10 13:59:03 +0530

    fix: footer responsive

--- src/components/comman/Footer.vue
+++ src/components/comman/Footer.vue
@@ -1,67 +1,64 @@
 <template>
   <div class="flex items-center justify-center relative overflow-x-hidden">
-   
-
- 
-
     <!-- Footer Content -->
-    <div class="w-full  h-auto lg:h-[610px] flex flex-col lg:flex-row justify-start items-end relative z-10">
+    <div class="w-full h-auto xl:h-[610px] flex flex-col xl:flex-row justify-start items-end relative z-10">
       <!-- Left Section -->
       <div
-        class="w-full lg:w-1/2 h-auto lg:h-[610px] px-10 lg:px-[100px] py-20 lg:py-[85px] bg-[#339abc] lg:rounded-tr-xl flex flex-col justify-start items-start gap-2.5 overflow-hidden mobile-no-radius">
+        class="w-full xl:w-1/2 h-auto xl:h-[610px] px-10 xl:px-[100px] py-20 xl:py-[85px] bg-[#339abc] xl:rounded-tr-xl flex flex-col justify-start items-start gap-2.5 overflow-hidden mobile-no-radius">
         <div class="w-full h-auto flex flex-col justify-start items-start gap-10">
           <div class="w-full h-auto flex flex-col justify-start items-center gap-4">
-            <div class="w-full text-white text-3xl lg:text-[54px] font-bold leading-tight lg:leading-[81px]">
+            <div class="w-full text-white text-3xl xl:text-[54px] font-bold leading-tight xl:leading-[81px]">
               Ready to transform your career or elevate your team?
             </div>
           </div>
           <button class="px-8 py-3.5 rounded-lg border-2 border-white flex justify-center items-center gap-4 overflow-hidden
-         hover:bg-white transition duration-300 text-white font-bold hover:text-[#339abc]">Let’s Connect
-</button>
+         hover:bg-white transition duration-300 text-white font-bold hover:text-[#339abc]">
+            Let’s Connect
+          </button>
         </div>
       </div>
 
       <!-- Right Section -->
       <div
-        class="w-full lg:w-1/2 lg:flex-1 px-6 lg:px-[60px] py-6 lg:py-[30px] bg-[#EBF5F8] lg:rounded-tr-lg flex flex-col justify-center items-start gap-10 overflow-hidden">
+        class="w-full xl:w-1/2 xl:flex-1 px-6 xl:px-[60px] py-6 xl:py-[30px] bg-[#EBF5F8] xl:rounded-tr-lg flex flex-col justify-center items-start gap-10 overflow-hidden">
         <div class="w-full flex flex-col justify-start items-start gap-8">
           <img class="w-[155px] h-[45.27px]" src="/public/img/icons/color_logo.png" alt="Logo" />
         </div>
         <div class="w-full h-auto flex flex-col justify-start items-start gap-8">
-          <div class="w-full flex flex-col lg:flex-row justify-between items-start gap-8">
+          <div class="w-full flex flex-col xl:flex-row justify-between items-start gap-8">
             <!-- Pages Section -->
             <div class="flex flex-col justify-start items-start gap-5">
-              <div class="text-[#222222] text-2xl font-semibold ">Pages</div>
-              <div class="text-[#222222] text-xl font-medium ">IT Staffing</div>
-              <div class="text-[#222222] text-xl font-medium ">IT Solutions Reseller</div>
-              <div class="text-[#222222] text-xl font-medium ">About us</div>
-              <div class="text-[#222222] text-xl font-medium ">Privacy policy</div>
+              <div class="text-[#222222] text-2xl font-semibold">Pages</div>
+              <div class="text-[#222222] text-xl font-medium">IT Staffing</div>
+              <div class="text-[#222222] text-xl font-medium">IT Solutions Reseller</div>
+              <div class="text-[#222222] text-xl font-medium">About us</div>
+              <div class="text-[#222222] text-xl font-medium">Privacy policy</div>
             </div>
 
             <!-- Jobs Section -->
             <div class="flex flex-col justify-start items-start gap-5">
-              <div class="text-[#222222] text-2xl font-semibold ">Jobs</div>
+              <div class="text-[#222222] text-2xl font-semibold">Jobs</div>
               <div class="flex justify-start items-center gap-2">
-                <span class="text-[#222222] text-xl font-medium ">Job Portal</span>
+                <span class="text-[#222222] text-xl font-medium">Job Portal</span>
               </div>
-              <div class="text-[#222222] text-xl font-medium ">Referrals</div>
+              <div class="text-[#222222] text-xl font-medium">Referrals</div>
             </div>
           </div>
 
           <!-- Contact Section -->
           <div class="w-full flex flex-col justify-start items-start gap-8">
-            <div class="w-full flex flex-col lg:flex-row justify-between gap-8">
+            <div class="w-full flex flex-col xl:flex-row justify-between gap-8">
               <div class="flex items-center gap-4">
                 <img class="w-7 h-7" src="/public/img/icons/mail.svg" alt="Email Icon" />
-                <span class="text-[#222222] text-lg font-medium ">admin@triuneinfomatics.com</span>
+                <span class="text-[#222222] text-lg font-medium">admin@triuneinfomatics.com</span>
               </div>
               <div class="flex gap-4">
                 <img class="w-7 h-7" src="/public/img/icons/phone-calling.svg" alt="Phone Icon" />
-                <span class="text-[#222222] text-lg font-medium ">(510) 854-8766</span>
+                <span class="text-[#222222] text-lg font-medium">(510) 854-8766</span>
               </div>
             </div>
 
-            <div class="w-full flex flex-col lg:flex-row justify-between gap-8">
+            <div class="w-full flex flex-col xl:flex-row justify-between gap-8">
               <div class="flex items-center gap-4">
                 <img class="w-7 h-7" src="/public/img/icons/location.svg" alt="Location Icon" />
                 <span class="text-[#222222] text-lg font-medium">
@@ -100,13 +97,13 @@
 
 <script>
 export default {
-  name: "ResponsiveLayout",
+  name: "ResponsiveFooter",
 };
 </script>
 
 <style scoped>
 /* Remove border radius on mobile view for the left section */
-@media (max-width: 1023px) {
+@media (max-width: 1249px) {
   .mobile-no-radius {
     border-radius: 0 !important;
   }
